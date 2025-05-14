d41af02eb9e209fd8ae47a2f48d501bad271e16e
= <ofcskn.omer@gmail.com>
2025-05-14 22:07:14 +0300

    Add receive_json, send_json, sanitize_filename and generate_unique_filename functions.

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,10 @@
 import hashlib
 import os
+import json
+import datetime
+import random
+import re
+import string
 
 def hash_password(password: str) -> str:
     return hashlib.sha256(password.encode()).hexdigest()
@@ -11,3 +16,35 @@ def ensure_folder_exists(path):
         print(f"✅ Folder created: {path}")
     else:
         print(f"📂 Folder already exists: {path}")
+
+def send_json(sock, data):
+    try:
+        message = json.dumps(data).encode()
+        sock.sendall(message)
+    except Exception as e:
+        print(f"[ERROR] Failed to send data: {e}")
+
+def recv_json(sock):
+    try:
+        data = sock.recv(4096)
+        return json.loads(data.decode())
+    except (ConnectionError, json.JSONDecodeError) as e:
+        print(f"[RECEIVE ERROR] {e}")
+        return None 
+
+    except Exception as e:
+        print(f"[RECEIVE UNEXPECTED ERROR] {e}")
+        return None
+
+def sanitize_filename(filename: str) -> str:
+    return re.sub(r'[^A-Za-z0-9._-]', '_', filename)
+
+def generate_unique_filename(owner_username: str, filename: str) -> str:
+    now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+    random_part = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+
+    safe_filename = sanitize_filename(filename)
+
+    unique_filename = f"{owner_username}-{safe_filename}-{now}-{random_part}"
+    return unique_filename
