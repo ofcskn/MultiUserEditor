7d9fadaf4d89cfd56125fb4b23e4e5939a50eb06
= <ofcskn.omer@gmail.com>
2025-05-14 22:08:03 +0300

    Use error handling for server.py

--- server/server.py
+++ server/server.py
@@ -20,11 +20,18 @@ def start_server():
     ensure_folder_exists(DATA_FOLDER)
     ensure_folder_exists(SAVE_FOLDER)
 
-    # Start a socket for a client
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        print("[+] Server listening...")
-        while True:
-            conn, addr = s.accept()
-            threading.Thread(target=handle_client, args=(conn, addr), daemon=True).start()
+    try:
+        # Start a socket for a client
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            print("[+] Server listening...")
+            while True:
+                conn, addr = s.accept()
+                threading.Thread(target=handle_client, args=(conn, addr), daemon=True).start()
+
+    except OSError as e:
+        if e.errno == 98:  # Address already in use
+            print("Error: Address already in use. Please try a different port.")
+        else:
+            print(f"An error occurred: {e}")
