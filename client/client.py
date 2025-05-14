380da2e2db2779b5ef6f5274fb5a3d81753a4912
= <ofcskn.omer@gmail.com>
2025-05-14 22:05:06 +0300

    Refactor client.py

--- client/client.py
+++ client/client.py
@@ -1,11 +1,9 @@
 import sys
-import threading
 from PySide6.QtWidgets import (QApplication)
 from client.views.login_view import LoginWindow
 from client.session import AppSession
 
 session = AppSession()
-lock = threading.Lock()
 
 def start_client():
     app = QApplication(sys.argv)
