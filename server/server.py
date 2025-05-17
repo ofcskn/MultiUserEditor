203e9c0be531c1b2356175cd1619f73445ee5e41
= <ofcskn.omer@gmail.com>
2025-05-17 11:07:00 +0300

    Fix the bug in server.py

--- server/server.py
+++ server/server.py
@@ -7,6 +7,9 @@ import os
 from core.utils import ensure_folder_exists 
 
 def start_server():
+    ensure_folder_exists(DATA_FOLDER)
+    ensure_folder_exists(SAVE_FOLDER)
+    
     # If the files.json is not valid, save it 
     if not os.path.exists(FILES_JSON) or os.stat(FILES_JSON).st_size == 0:
         with open(FILES_JSON, 'w') as f:
@@ -16,9 +19,6 @@ def start_server():
     if not os.path.exists(USERS_JSON) or os.stat(USERS_JSON).st_size == 0:
         with open(USERS_JSON, 'w') as f:
             json.dump([], f)
-
-    ensure_folder_exists(DATA_FOLDER)
-    ensure_folder_exists(SAVE_FOLDER)
 
     try:
         # Start a socket for a client
