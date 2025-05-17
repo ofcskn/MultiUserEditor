4248250b3c77e556c8c4b74a14de2e72c9ed0c5a
= <ofcskn.omer@gmail.com>
2025-05-17 15:03:47 +0300

    Update constansts.py

--- core/constants.py
+++ core/constants.py
@@ -1,33 +1,26 @@
 import os
 
-# Mesaj türleri
-MSG_LOGIN = "LOGIN"         # Kullanıcı sisteme giriş yaptı ya da kaydolu 
-MSG_FILE_UPDATE = "FILE_UPDATE"           # Dosya güncellemesi
-MSG_FILE_LIST = "FILES"     # Paylaşılan dosyalar listesi
-MSG_CREATE_FILE = "CREATE_FILE" # Yeni dosya oluşturma isteği
-MSG_FILE_LOAD = "LOAD"   # Dosya yükleme 
-MSG_FILE_LOAD_VIEWER = "LOAD_VIEWER" # Dosyayı sadece görüntüle 
-MSG_JOIN_FILE="JOIN_FILE"   # Kullanıcı dosyaya katıldı
-MSG_ERROR= "ERROR" # Herhangi bir hata
-MSG_PERMISSION_ERROR = "PERMISSON_ERROR" # İzin hatası
-MSG_LOGIN_ERROR = "LOGIN_ERROR" # Giriş yaparken alınan hata
-MSG_SUCCESS = "SUCCESS" # Başarılı bir aksiyon
-MSG_FILE_UPDATE_SUCCESS = "FILE_UPDATE_SUCCESS"
-MSG_FILE_UPDATE_ERROR = "FILE_UPDATE_ERROR"
-MSG_USER_ACTIVE_SESSION = "USER_ACTIVE_SESSION"
-MSG_FILES_PAGE_REDIRECT= "FILES_PAGE_REDIRECT"
-MSG_FILE_LIST_UPDATE = "FILE_LIST_UPDATE"
-
-# Paket ayırıcı (protokol için)
-DELIMITER = "|"
-
-# Kullanıcı adı maksimum uzunluğu
-MAX_USERNAME_LEN = 16
+# Message types
+MSG_LOGIN = "LOGIN"                       # Login
+MSG_FILE_UPDATE = "FILE_UPDATE"           # File update
+MSG_FILE_LIST = "FILES"                   # List of shared files
+MSG_CREATE_FILE = "CREATE_FILE"           # Request to create a new file
+MSG_CREATE_FILE_ERROR = "CREATE_FILE_ERROR" # If an error occurs when a file is created
+MSG_FILE_LOAD = "LOAD"                    # Load file
+MSG_FILE_LOAD_VIEWER = "LOAD_VIEWER"      # Load file as read-only/viewer
+MSG_JOIN_FILE = "JOIN_FILE"               # User joined the file
+MSG_ERROR = "ERROR"                       # General error
+MSG_PERMISSION_ERROR = "PERMISSION_ERROR" # Permission error
+MSG_LOGIN_ERROR = "LOGIN_ERROR"           # Login error
+MSG_SUCCESS = "SUCCESS"                   # Successful action
+MSG_FILE_UPDATE_SUCCESS = "FILE_UPDATE_SUCCESS" # If an error does not occurs when a file is updated
+MSG_FILE_UPDATE_ERROR = "FILE_UPDATE_ERROR" # If an error occurs when a file is updated
+MSG_USER_ACTIVE_SESSION = "USER_ACTIVE_SESSION" # If there is an active client with an username
+MSG_FILES_PAGE_REDIRECT = "FILES_PAGE_REDIRECT" # Redirect to files 
+MSG_FILE_LIST_UPDATE = "FILE_LIST_UPDATE" # Update the file list
 
 HOST = '127.0.0.1'
 PORT = 65433
-
-SAVE_INTERVAL = 5  # seconds
 
 DATA_FOLDER = 'data'
 SAVE_FOLDER = 'files'
